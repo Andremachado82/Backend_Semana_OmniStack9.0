07ea9e6d55ec9f4596289af33092f7f6aaedc949
André Machado <andrels.machado82@gmail.com>
2019-10-02 16:18:20 -0300

    Configurando ações da tela de Login

--- src/App.js
+++ src/App.js
@@ -3,25 +3,16 @@ import './App.css';
 
 import logo from './assets/logo.svg';
 
-function App() {
+import Routes from './routes'
+
+function App() { 
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
+
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para progrmadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-          id="email"
-          type="email"           
-          placeholder="Seu melhor e-mail"
-          />
-
-          <button className="btn" type="submit" >Entrar</button>
-        </form>
+        <Routes />
+        
       </div>
     </div>    
   );
